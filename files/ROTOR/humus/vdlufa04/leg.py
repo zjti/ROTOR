6bcf0f841a710a6ed860d3448d84a06914db7877
zjti <you@example.com>
2025-08-04 08:01:44 +0000

    Updates

--- files/ROTOR/humus/vdlufa04/leg.py
+++ files/ROTOR/humus/vdlufa04/leg.py
@@ -15,15 +15,8 @@ class FTTR_ERBSE(VDLUFA):
 class SOJA(VDLUFA):
     HP_HÄ_per_ha = 160
     NP_HÄ_per_ha = 0
-<<<<<<< HEAD
     
-
-class LEG_GRAS(VDLUFA):
-    HP_HÄ_per_ha = 160
-    NP_HÄ_per_ha = 0
-=======
 
 class LEG_GRAS(VDLUFA):
     HP_HÄ_per_ha = 800
     NP_HÄ_per_ha = 0
->>>>>>> ec033afbd83317f2e9290f2336bf5d89ad7dcf40
